911c318a41fdb77185ad62a9a36f9ce506a8e36c
Courtney Phillips <courtney@epicodus.com>
2017-05-25 14:25:59 -0700

    Call API request asynchronously to avoid clogging primary thread.

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantService.java
@@ -1,13 +1,20 @@
 package com.epicodus.myrestaurants;
 import okhttp3.Request;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
 
 public class RestaurantService {
     static String apiUrl = "http://138.197.214.133/api/v1/foodtruck";
 
-    public static void findRestaurants(String location){
+    public static void findRestaurants(String location, Callback callback){
         Request request= new Request.Builder()
                 .url(apiUrl)
                 .build();
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 
 }
